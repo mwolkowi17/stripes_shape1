1c1d6fb217875d424cf1d5878ec8dd39208438c2
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2022-04-25 19:22:05 +0200

    cleaning

--- src/ModelAnim.js
+++ src/ModelAnim.js
@@ -6,32 +6,36 @@ import { OrbitControls } from "@react-three/drei";
 import { useEffect, useRef, useState } from 'react';
 
 
+
 export function ModelAnim(props) {
 
   const mixer = useRef();
   const gltf = useLoader(GLTFLoader, 'deform_strips6_10anim.gltf')
 
-  const [startPlay,setStartPlay]=useState(false)
+  const [startPlay, setStartPlay] = useState(false)
+  const [ifStarted, setIfStarted] = useState(false)
   //animatio segment start
+  const ifPlayed = useRef();
+  //ifPlayed.current=false;
   useEffect(() => {
-    if (gltf) {
-      mixer.current = new THREE.AnimationMixer(gltf.scene)
-      const action = mixer.current.clipAction(gltf.animations[0])
-      //console.log(gltf.animations)
-      //action.play()
-     
-      if(startPlay===true){
-        action.play()
-        if(mixer.current.time>2){
-          mixer.current.timeScale=0
-          console.log('stop')
-        }
-        }
-       
+
+  if (gltf) {
+    mixer.current = new THREE.AnimationMixer(gltf.scene)
+    const action = mixer.current.clipAction(gltf.animations[0])
+    ifPlayed.current = false
+
+    if (startPlay) {
+    
+        action.play();
+        setTimeout(() => {
+        action.stop();
+        setStartPlay(false)
+        }, 6000);
         
-      
     }
-  }, [gltf, startPlay])
+
+  }
+  }, [gltf, startPlay, ifStarted])
 
   useFrame((state, delta) => {
     mixer.current?.update(delta)
@@ -42,7 +46,7 @@ export function ModelAnim(props) {
   return (
     <>
       <OrbitControls />
-      <primitive object={gltf.scene} dispose={null} onPointerMove={(e) => setStartPlay(true)} />
+      <primitive object={gltf.scene} dispose={null} onPointerOver={(e) => setStartPlay(true)}  />
     </>
   )
 }
